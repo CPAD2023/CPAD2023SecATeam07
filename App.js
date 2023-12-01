9e51c1de9c83039e5c7e881a912374d036c99dbf
kshitij-rajesh-rao <127507464+kshitij-rajesh-rao@users.noreply.github.com>
2023-12-01 01:20:46 +0100

    added package.json and app.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView,StyleSheet, Text, View } from 'react-native';
+import SignInScreen from './src/screens/SignInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.root}>
+      <SignInScreen />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  root:{
+    flex:1,
+    backgroundColor:"#F9FBFC"
+  }
+});
