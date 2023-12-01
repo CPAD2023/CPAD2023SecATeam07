0c7834605eec78eb2787bc864256683ebc6de191
kshitij-rajesh-rao <127507464+kshitij-rajesh-rao@users.noreply.github.com>
2023-12-01 01:31:58 +0100

    Update SignInScreen.js

--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -1,48 +1,39 @@
 import React, {useState} from "react";
-import { View,Text,StyleSheet} from "react-native";
+import { View,Text,Image,StyleSheet, useWindowDimensions } from "react-native";
+import favicon from '../../../assets/images/favicon.png'
 import CustomInput from '../../components/CustomInput';
 import CustomButton from "../../components/CustomButton";
-import { router } from 'expo-router';
 
-const SignUpScreen= () =>{
+
+const SignInScreen= () =>{
 
     const [username, setUserName]= useState('');
-    const [email, setEmail]= useState('');
     const [password, setPassword]= useState('');
-    const [passwordRepeat, setPasswordRepeat]= useState('');
 
-    const onRegisterPressed= () => {
-        console.warn("onRegisterPressed");
+    const {height} = useWindowDimensions();
+
+    const onSignInPressed= () => {
+        console.warn("Sign In");
+    }
+
+    const onForgotPasswordPressed= () => {
+        console.warn("onForgotPasswordPressed");
     }
 
     const onSignUpPressed= () => {
         console.warn("onSignUpPressed");
     }
 
-    const onSignInPressed= () => {
-        console.warn("onSignInPressed");
-    }
-
-    const onTermsOfUsePressed = () => {
-        console.warn("onTermsOfUsePressed");
-    }
-
-    const onPrivacyPressed = () => {
-        console.warn("onPrivacyPressed");
-    }
-
-
     return (
         <View style={styles.root}>
-            <Text style={styles.title}>Create an account</Text>
-            <CustomInput placeholder={"Username"} value={username} setValue={setUserName} />
-            <CustomInput placeholder={"Email"} value={email} setValue={setEmail} />
-            <CustomInput placeholder={"Password"} value={password} setValue={setPassword} secureTextEntry={true}/>
-            <CustomInput placeholder={"Repeat Password"} value={passwordRepeat} setValue={setPasswordRepeat} secureTextEntry={true}/>
+            <Image source={favicon} style={[styles.logo,{height:height*0.3}]} resizeMode="contain" />
 
-            <CustomButton text="Register" onPress= {onRegisterPressed}/>
-            <Text style={styles.text}>By registering, you confirm that you accept our <Text style={styles.link} onPress={onTermsOfUsePressed}>Terms of use </Text>and <Text style={styles.link} onPress={onPrivacyPressed}>Privacy Policy</Text></Text>
-            <CustomButton text="Have an account? Sign in" onPress= {onSignInPressed} type="TERTIARY"/>
+            <CustomInput placeholder={"Username"} value={username} setValue={setUserName} />
+            <CustomInput placeholder={"Password"} value={password} setValue={setPassword} secureTextEntry={true}/>
+
+            <CustomButton text="Sign in" onPress= {onSignInPressed}/>
+            {/* <CustomButton text="Forgot Password" onPress= {onForgotPasswordPressed} type="TERTIARY"/> */}
+            <CustomButton text="Don't have an account? Create one" onPress= {onSignUpPressed} type="TERTIARY"/>
         </View>
     );
 };
@@ -52,21 +43,13 @@ const styles=StyleSheet.create({
         alignItems:'center',
         padding:20
     },
-    title:{
-        fontSize:24,
-        fontWeight:"bold",
-        color:"#051C60",
-        margin: 10
+    logo:{
+        marginBottom:"20%",
+        width:"70%",
+        maxWidth: 300,
+        maxHeight:200,
     },
-    text:{
-        color:"gray",
-        marginVertical:10
-
-    },
-    link:{
-        color:"#FDB075"
-    }
 
 })
 
-export default SignUpScreen;
+export default SignInScreen;
